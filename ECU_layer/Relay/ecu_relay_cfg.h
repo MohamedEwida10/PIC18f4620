14e481c0c7a3f0555cae72ac0d1188dee2be0364
MohamedEwida10 <https://github.com/MohamedEwida10>
2024-02-01 00:14:49 +0200

    PIC18f4620 interfaces

---
+++ ECU_layer/Relay/ecu_relay_cfg.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   ecu_relay_cfg.h
+ * Author: mohamed.ewida
+ *
+ * Created on October 2, 2023, 6:50 AM
+ */
+
+#ifndef ECU_RELAY_CFG_H
+#define	ECU_RELAY_CFG_H
+
+
+#endif	/* ECU_RELAY_CFG_H */
+
